2c180bb7635263e21649b8160a2f8df07d864b69
Pasindu Thiwanka <pasindu.17@itfac.mrt.ac.lk>
2020-07-10 19:35:04 +0530

    Intial Commit Intial Commit

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const App = function () {
 
     return (
         <div>
-            <label className="label" for="name">{labelText}</label>
+            <label className="label" htmlFor="name">{labelText}</label>
             <input id="name" type="text"/>
             <button style={{backgroundColor:"blue",color:"white"}}>{buttonText.text}</button>
         </div>
