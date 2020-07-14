9e9821d26db7e35a5e74ddc140445ca8f4b22696
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2020-07-14 11:39:59 +0530

    Practice - FB Comment Update

--- index.js
+++ index.js
@@ -2,35 +2,49 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import faker from 'faker'
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <div className ="comment">
+            <ApprovalCard>
+                <div>
+                    <h4>Warning !</h4>
+                </div>
+                Are You Sure ?
+            </ApprovalCard>
+            <ApprovalCard>
                 <CommentDetail
                     author='Sam'
-                    timeAgo ="Today 4:45 pm"
-                    content ='Its beautiful'
-                    image ={faker.image.avatar()}
+                    timeAgo="Today 4:45 pm"
+                    content='Its beautiful'
+                    image={faker.image.avatar()}
                 />
+            </ApprovalCard>
 
+
+            <ApprovalCard>
                 <CommentDetail
-                    author ='Alex'
-                    timeAgo ="Today 2:34 pm"
-                    content ='Good Looking'
-                    image ={faker.image.avatar()}
+                    author='Alex'
+                    timeAgo="Today 2:34 pm"
+                    content='Good Looking'
+                    image={faker.image.avatar()}
                 />
+            </ApprovalCard>
 
+
+            <ApprovalCard>
                 <CommentDetail
-                    author ='Jane'
-                    timeAgo =" Yesterday 11:07 pm"
-                    content ='Very Cool'
-                    image ={faker.image.avatar()}
+                    author='Jane'
+                    timeAgo=" Yesterday 11:07 pm"
+                    content='Very Cool'
+                    image={faker.image.avatar()}
                 />
-
-            </div>
+            </ApprovalCard>
 
         </div>
+
+
     )
 }
 
