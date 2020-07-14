4ec31627adf685e420cb59b176b08a215d069fb2
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2020-07-14 14:03:02 +0530

    Practice - Seasons

--- index.js
+++ index.js
@@ -1,51 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import faker from 'faker'
-import CommentDetail from "./CommentDetail";
-import ApprovalCard from "./ApprovalCard";
-
-const App = () => {
-    return (
-        <div className="ui container comments">
-            <ApprovalCard>
-                <div>
-                    <h4>Warning !</h4>
-                </div>
-                Are You Sure ?
-            </ApprovalCard>
-            <ApprovalCard>
-                <CommentDetail
-                    author='Sam'
-                    timeAgo="Today 4:45 pm"
-                    content='Its beautiful'
-                    image={faker.image.avatar()}
-                />
-            </ApprovalCard>
 
 
-            <ApprovalCard>
-                <CommentDetail
-                    author='Alex'
-                    timeAgo="Today 2:34 pm"
-                    content='Good Looking'
-                    image={faker.image.avatar()}
-                />
-            </ApprovalCard>
+class App extends React.Component {
 
+    render() {
 
-            <ApprovalCard>
-                <CommentDetail
-                    author='Jane'
-                    timeAgo=" Yesterday 11:07 pm"
-                    content='Very Cool'
-                    image={faker.image.avatar()}
-                />
-            </ApprovalCard>
-
-        </div>
-
-
-    )
+        //we can use this  to identify the location of the user
+        window.navigator.geolocation.getCurrentPosition(
+            (position)=>console.log(position),
+            (err) =>console.log(err)
+        )
+        return (<div>Latitide :</div>)
+    }
 }
 
-ReactDOM.render(<App/>, document.querySelector('#root'))
+ReactDOM.render(
+    <App />, document.querySelector('#root')
+)
