16bc218de23de2a30ccd2b76418acc0c647389fd
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2020-08-03 16:29:13 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,49 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import SeasonDisplay from "./SeasonDisplay";
-import Spinner from './Spinner'
-
-class App extends React.Component {
-    state = {lat: null,errorMessage:''}
-
-    componentDidMount(){
-        //we can use this  to identify the location of the user
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                //update we need to call  setState
-                this.setState({lat: position.coords.latitude})
-            },
-            (err) => {
-                this.setState(
-                    {errorMessage: err.message}
-                )
-            }
-        )
-
-
-    }
-
-
-
-    
-
-    render() {
-        if (this.state.errorMessage&&!this.state.lat){
-            return (<div>Error :{this.state.errorMessage}</div>)
-        }
-        
-        if (!this.state.errorMessage && this.state.lat){
-            return (<SeasonDisplay lat={this.state.lat}/>)
-        }
-        return <Spinner />
-
-    }
-
-
-}
-
+import React from "react";
+import ReactDOM from "react-dom"
+import App from "./components/App"
 
 
 ReactDOM.render(
-    <App/>, document.querySelector('#root')
+    <App />,
+    document.querySelector('#root')
 )
