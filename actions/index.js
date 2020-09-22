14633ce2262008c3ad2c9b3ba36a2f19964b8ca6
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2020-09-22 16:19:50 +0530

    Song_example_redux

--- actions/index.js
+++ actions/index.js
@@ -1,6 +1,8 @@
 export const selectSong =(song)=>{
-    return {
-        type :'SONG_SELECTED',
-        payload:song
+    return{
+        type:'SONG_SELECTED',
+        payload: song
     }
 }
+
+
--- reducers/index.js
+++ reducers/index.js
@@ -1,26 +1,23 @@
-import {combineReducers} from 'redux';
+import {combineReducers} from 'redux'
 
 const songsReducer =()=>{
-   return [
-
-       {title: 'No Scrubs', duration: '4:05'},
-       {title: 'Marcrena', duration: '3:45'},
-       {title: 'All Star', duration: '3:27'},
-       {title: 'I Want In that Way', duration: '5:16'}
-
-   ]
+    return [
+        {title:'No Scrubs',duration :'4:05'},
+        {title:'Marcherina',duration :'3:25'},
+        {title:'i want in that way',duration :'4:43'},
+        {title:"all start",duration :'5:14'},
+    ]
 }
 
-const selectedSongReducer=(selectedSong=null,action)=>{
-    if (action.type==="SONG_SELECTED"){
+const selectedSongReducer=(selectedSong =null,action)=>{
+    if(action.type==="SONG_SELECTED"){
         return action.payload
     }
 
     return selectedSong
-
 }
 
 export default combineReducers({
-    songs: songsReducer,
-    selectedSong: selectedSongReducer
+    songs:songsReducer,
+    selectedSong :selectedSongReducer
 })
