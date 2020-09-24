763d01df5b056d7fc9c5ae3394941b686503d9f9
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2020-09-24 18:19:44 +0530

    Blog Sample Project

--- actions/index.js
+++ actions/index.js
@@ -1,8 +1,16 @@
-export const selectSong =(song)=>{
-    return{
-        type:'SONG_SELECTED',
-        payload: song
+import jsonPlaceHolder from "../apis/jsonPlaceHolder";
+
+export const fetchPosts = ()=> async dispatch => {
+        const response = await jsonPlaceHolder.get('/posts')
+
+        dispatch({type: "FETCH_POSTS", payload: response.data})
+
     }
+
+
+export const fetchUser = (id)=> async dispatch => {
+    const response = await jsonPlaceHolder.get(`/users/${id}`)
+
+    dispatch({type: "FETCH_USER", payload: response.data})
+
 }
-
-
--- reducers/index.js
+++ reducers/index.js
@@ -1,23 +1,11 @@
 import {combineReducers} from 'redux'
-
-const songsReducer =()=>{
-    return [
-        {title:'No Scrubs',duration :'4:05'},
-        {title:'Marcherina',duration :'3:25'},
-        {title:'i want in that way',duration :'4:43'},
-        {title:"all start",duration :'5:14'},
-    ]
-}
-
-const selectedSongReducer=(selectedSong =null,action)=>{
-    if(action.type==="SONG_SELECTED"){
-        return action.payload
-    }
-
-    return selectedSong
-}
+import postReducer from "./postReducer";
+import usersReducers from "./usersReducers";
 
 export default combineReducers({
-    songs:songsReducer,
-    selectedSong :selectedSongReducer
+    posts :postReducer,
+    users :usersReducers,
 })
+
+
+
