2b47bc1d452aec7354ea0d746cca8a9648da3799
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2020-10-02 08:33:50 +0530

    season_1 _index

--- index.js
+++ index.js
@@ -1,49 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import CommentDetail from "./CommentDetail";
-import faker from 'faker'
-import ApprovalCard from "./ApprovalCard";
 
 const App =()=>{
-    return(
-        <div className ='ui container comments'>
-            <ApprovalCard>
-                <h4> Warning </h4>
-                <div >Are You Sure Want to do this ?</div>
-
-            </ApprovalCard>
-            <ApprovalCard>
-                <CommentDetail
-                    author ="sam"
-                    timeAgo ="Today at 3.39pm"
-                    comment ="nice work"
-                    imgSrc={faker.image.avatar()}
-                />
-            </ApprovalCard>
-
-
-            <ApprovalCard>
-                <CommentDetail
-                    author ="alex"
-                    timeAgo='yesterday 5.54am'
-                    comment ="good looking"
-                    imgSrc={faker.image.avatar()}
-                />
-            </ApprovalCard>
-
-
-            <ApprovalCard>
-                <CommentDetail
-                    author ="jane"
-                    timeAgo='toady at 4.30pm '
-                    comment ="very beautiful"
-                    imgSrc={faker.image.avatar()}
-
-                />
-            </ApprovalCard>
-
-        </div>
-    )
+    return (
+        <div>
+            App
+        </div>)
 }
 
 ReactDOM.render(<App />,document.querySelector("#root"))
