bf87b6857e15719a9cb81ac0cebfdbe131a82bb3
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2020-11-13 14:02:59 +0530

    Blogs Complete Project

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,22 +1,9 @@
+import React from "react"
 import {combineReducers} from "redux";
-
-const songsReducer =()=>{
-    return[
-        {title:"No Srubs",duration :"4:05"},
-        {title:"Marcerena",duration :"3:25"},
-        {title:"All Star",duration :"4:53"},
-        {title:"I wank in That way",duration :"3:32"},
-    ]
-}
-
-const selectedSongReducer = (selectedSong =null,action) =>{
-    if(action.type==="SONG_SELECTED"){
-      return action.payload
-    }
-return selectedSong
-}
+import postsReducer from "./postsReducer";
+import usersReducer from "./usersReducer";
 
 export default combineReducers({
-    songs:songsReducer,
-    selectedSong:selectedSongReducer
-})
+    posts :postsReducer,
+    users:usersReducer
+});
