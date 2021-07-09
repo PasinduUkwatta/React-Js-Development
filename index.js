3182dfbccc86aaf4262211f473ce83e6d6821809
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2021-07-09 19:44:22 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider,connect} from "react-redux";
+import {createStore,applyMiddleware,combineReducers} from "redux";
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './containers/App';
+import registerServiceWorker from './registerServiceWorker';
+import 'tachyons';
+import {searchRobots,requestRobots} from "./containers/reducers";
+import {createLogger} from "redux-logger/src";
+import thunk from "redux-thunk";
+import thunkMiddleware from "redux-thunk";
+
+const logger =createLogger()
+const rootReducer =combineReducers({searchRobots,requestRobots})
+const store =createStore(rootReducer,applyMiddleware(thunkMiddleware))
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App  />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(console.log);
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('root'));
+registerServiceWorker();
