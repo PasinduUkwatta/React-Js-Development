b28df0e0150eb118afea16988d344e195617dbe3
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2021-10-01 11:54:58 +0530

    Expense App

--- src/App.js
+++ src/App.js
@@ -1,42 +1,16 @@
-import React from 'react';
 import './App.css';
-import {CardList} from "./components/card-list/card-list.component";
-import {SearchBox} from "./components/search-box/search-box.component";
+import ExpenseItem from "./components/ExpenseItem";
 
 
-class App extends React.Component{
-  constructor() {
-    super ();
-    this.state = {
-        monsters:[],
-        searchField:''
-    }
-  }
-  
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response=>response.json())
-        .then(users =>this.setState({monsters: users}))
-  }
-  
-  render() {
-
-    const { monsters,searchField } = this.state;
-    const filterdMonsters =monsters.filter(monster =>
-        monster.name.toLowerCase().includes(searchField.toLowerCase())
-    )
-    return (
-        <div className="App">
-          <SearchBox
-          placeholder ='Search Monsters'
-          handleChange ={e => this.setState({searchField: e.target.value})}
-          />
-
-          <CardList monsters={filterdMonsters}/>
-        </div>
-    );
-  }
-
+function App() {
+  return (
+    <div>
+      <h2>Let's get Started</h2>
+      <p>This is also visible</p>
+        <ExpenseItem/>
+        <ExpenseItem/>
+    </div>
+  );
 }
 
 export default App;
