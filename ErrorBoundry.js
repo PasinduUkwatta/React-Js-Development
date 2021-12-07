d2a3c8c617687ec43631245c22fd3bcd1a934e5d
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2021-12-07 08:40:39 +0530

    Add files via upload

--- ErrorBoundry.js
+++ ErrorBoundry.js
@@ -1,80 +1,22 @@
-import React from "react"
-import {ErrorDetails} from "./UserFuctions";
+import React from "react";
 
+class ErrorBoundry extends React.Component {
+  constructor() {
+    super();
+    this.state = { hasError: false };
+  }
 
-class ErrorBoundry extends React.Component{
+  componentDidCatch(error, errorInfo) {
+    console.log(error);
+    this.setState({ hasError: true });
+  }
 
-constructor(props) {
-    super(props);
-
-    this.state={
-        hasError:false,
-        error:"",
-        errorInfo :""
-
-
+  render() {
+    if (this.state.hasError) {
+      return <p>Something went wrong!</p>;
     }
-    this.changeHandler = this.changeHandler.bind(this);
-
-
-
-}
-    changeHandler = e => {
-        this.setState({[e.target.name]: e.target.value})
-
-    }
-
-
-    static getDerivedStateFromError(error){
-        return{
-            hasError:true
-
-        }
-    }
-
-    componentDidCatch(error, errorInfo,componentStack) {
-    console.log(error)
-    console.log(componentStack)
-
-        let errorString =error.toString()
-    console.log(errorInfo.componentStack)
-        console.log(errorString)
-        console.log(typeof(errorString))
-        this.setState({error:errorInfo.componentStack})
-
-        console.log(error.stack)
-
-
-
-
-
-        const errorDetails={
-         error:errorInfo.componentStack,
-         errorInfo:errorString
-
-
-        }
-
-
-
-        console.log(errorDetails)
-
-         // ErrorDetails(errorDetails).then(res=>{})
-
-
-
-
-    }
-
-    render() {
-
-    if(this.state.hasError){
-      return <h1>Something went Wrong!</h1>
-    }
-
-    return this.props.children
-
-    }
+    return this.props.children;
+  }
 }
 
-export default ErrorBoundry
+export default ErrorBoundry;
