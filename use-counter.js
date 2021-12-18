b1fad556f2c73f13d4d06152c28fd42ee6ac3ec7
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2021-12-18 08:31:39 +0530

    Add files via upload

---
+++ use-counter.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from "react";
+
+const useCounter = (forwads = true) => {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (forwads) {
+        setCounter((prevCounter) => prevCounter + 1);
+      } else {
+        setCounter((prevCounter) => prevCounter - 1);
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [forwads]);
+  return counter;
+};
+
+export default useCounter;
