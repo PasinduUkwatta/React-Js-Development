ade22ad9bf7ec367b3329d73350066af7f0ed7fc
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2022-01-11 08:59:19 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,15 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import Counter from './components/Counter';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-          <p>Happy New Year 2022 !</p>
-          <p>All the best for the New Year !</p>
-      </header>
-    </div>
+    <Counter />
   );
 }
 
