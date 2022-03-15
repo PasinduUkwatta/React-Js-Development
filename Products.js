edf0f405ec226e0533a7f947c57394d9337e1e54
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2022-03-15 07:30:11 +0530

    Add files via upload

--- Products.js
+++ Products.js
@@ -1,5 +1,27 @@
+import { Link } from "react-router-dom";
+
 const Products = () => {
-  return <div>The Products Page</div>;
+  return (
+    <div>
+      <section>
+        {" "}
+        <h1>The Products Page</h1>
+        <ul>
+          <li>
+            <Link to="/products/p1">Book</Link>
+          </li>
+          <li>
+            {" "}
+            <Link to="/products/p2">Carpet</Link>
+          </li>
+          <li>
+            {" "}
+            <Link to="/products/p3">Online Course</Link>
+          </li>
+        </ul>
+      </section>
+    </div>
+  );
 };
 
 export default Products;
--- Welcome.js
+++ Welcome.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { Route } from "react-router-dom";
 
 const Welcome = () => {
   return (
     <div>
       <h1>The Welcome Page</h1>
+      <Route path="/welcome/new-user">
+        <p>Welcome new user!</p>
+      </Route>
     </div>
   );
 };
