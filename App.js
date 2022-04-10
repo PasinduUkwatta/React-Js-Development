26015090a0eacde60e922bf354397d6cccaa5ae0
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2022-04-10 07:17:50 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,17 +1,52 @@
-import Cart from "./components/Cart/Cart";
-import Layout from "./components/Layout/Layout";
-import Products from "./components/Shop/Products";
-import { useSelector } from "react-redux";
+import React,{useState} from "react";
+import './App.css';
+import ExpenseItem from "./components/Expenses/ExpenseItem";
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
-function App() {
-  const showCart = useSelector((state) => state.ui.cartIsVisibale);
+const DUMMY_EXPENSES= [
+    {
+        id: 'e1',
+        title: 'Toilet Paper',
+        amount: 94.12,
+        date: new Date(2020, 7, 14),
+    },
+    {id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12)},
+    {
+        id: 'e3',
+        title: 'Car Insurance',
+        amount: 294.67,
+        date: new Date(2021, 2, 28),
+    },
+    {
+        id: 'e4',
+        title: 'New Desk (Wooden)',
+        amount: 450,
+        date: new Date(2021, 5, 12),
+    },
+];
 
-  return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
-  );
+const App =() =>{
+
+    const [expenses,setExpenses]= useState(DUMMY_EXPENSES)
+
+    const addExpenseHandler=expense=>{
+        setExpenses((prevExpenses) =>{
+            return [expense,...prevExpenses]
+        })
+    }
+
+
+
+    return (
+        <div>
+            <body className="body">
+            <NewExpense onAddExpense={addExpenseHandler}/>
+            <Expenses items={expenses}/>
+            </body>
+
+        </div>
+    );
 }
 
 export default App;
