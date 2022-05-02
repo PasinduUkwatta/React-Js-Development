6da5e63d22afd70ece63dc166a040bc09738ccf8
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2022-05-02 11:06:14 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,52 +1,28 @@
-import React,{useState} from "react";
-import './App.css';
-import ExpenseItem from "./components/Expenses/ExpenseItem";
-import Expenses from "./components/Expenses/Expenses";
-import NewExpense from "./components/NewExpense/NewExpense";
+import React, { useState } from "react";
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
-const DUMMY_EXPENSES= [
-    {
-        id: 'e1',
-        title: 'Toilet Paper',
-        amount: 94.12,
-        date: new Date(2020, 7, 14),
-    },
-    {id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12)},
-    {
-        id: 'e3',
-        title: 'Car Insurance',
-        amount: 294.67,
-        date: new Date(2021, 2, 28),
-    },
-    {
-        id: 'e4',
-        title: 'New Desk (Wooden)',
-        amount: 450,
-        date: new Date(2021, 5, 12),
-    },
-];
+function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
 
-const App =() =>{
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
 
-    const [expenses,setExpenses]= useState(DUMMY_EXPENSES)
-
-    const addExpenseHandler=expense=>{
-        setExpenses((prevExpenses) =>{
-            return [expense,...prevExpenses]
-        })
-    }
-
-
-
-    return (
-        <div>
-            <body className="body">
-            <NewExpense onAddExpense={addExpenseHandler}/>
-            <Expenses items={expenses}/>
-            </body>
-
-        </div>
-    );
+  return (
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
+  );
 }
 
 export default App;
