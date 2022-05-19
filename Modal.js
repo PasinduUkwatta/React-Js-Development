e023e4ccb805ef5d0fce5ad931017e30cf64d74f
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2022-05-19 06:09:47 +0530

    Add files via upload

---
+++ Modal.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './Modal.css';
+
+const modal = (props) => {
+
+    const cssClasses =["Model",props.show ? "ModelOpen":"ModelClosed"]
+
+
+    return (
+        <div className={cssClasses.join(" ")}>
+            <h1>A Modal</h1>
+            <button className="Button" onClick={props.closed}>Dismiss</button>
+        </div>
+        )
+
+};
+
+export default modal;
