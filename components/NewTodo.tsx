c5f14a884c3b2cd7f01b3ae1bc87d6fb051db979
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2022-06-07 08:25:00 +0530

    Add files via upload

---
+++ components/NewTodo.tsx
@@ -0,0 +1,39 @@
+import React, {useContext, useRef} from "react";
+import classes from "./NewTodo.module.css";
+import {TodosContext} from "../store/todos-context";
+
+
+const NewTodo:React.FC =()=>{
+
+
+   const todosCtx=  useContext(TodosContext)
+
+    const todoTextInputRef =useRef<HTMLInputElement>(null)
+
+    const submitHandler =(event:React.FormEvent)=>{
+        event.preventDefault()
+
+        const enteredText =todoTextInputRef.current!.value
+
+
+        if(enteredText.trim().length===0){
+            return
+        }
+
+
+        todosCtx.addTodo(enteredText)
+    }
+
+
+    return(
+        <div>
+            <form onSubmit={submitHandler} className={classes.form}>
+                <label htmlFor="text">Todo Text</label>
+                <input type="text" id="text" ref={todoTextInputRef}/>
+                <button>Add Todo</button>
+            </form>
+        </div>
+    )
+}
+
+export default NewTodo
---
+++ components/TodoItem.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from "./TodoItem.module.css";
+
+const TodoItem :React.FC<{text:string;onRemoveTodo:()=>void}> =(props)=>{
+    return(
+        <div>
+            <li className={classes.item} onClick={props.onRemoveTodo}>{props.text}</li>
+        </div>
+    )
+}
+
+export default TodoItem
